ae86c8fb4da7588f818d511d436ee2e222a2350c
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-09 17:51:41 -0500

    feat: :sparkles: Versiones 1.0.0 , 1.0.1 y 1.1.0 de los metodos crud en la api
    Se crean las diferentes versiones de acceso para los diferentes metodos crud en los endpoints

---
+++ api/versions/1.0.1/Productov1.0.1.js
@@ -0,0 +1,69 @@
+import { con } from "../../db/atlas.js";
+
+let db= await con();
+let collection = db.collection("productos")
+let autoincrement = db.collection("autoincrement");
+
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.sequence_value;
+}
+
+export const getProductoV101 = async ( req,res)=>{
+    try {
+        if(!req.rateLimit) return;
+        let funtion= await collection.find({}).sort({ _id : 1}).toArray();
+        res.send(funtion)
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+export const postProductoV101 = async (req,res)=>{
+    try{
+        if(!req.rateLimit) return;
+        const id =  await increment("productos");
+        let data= {_id: id, ...req.body};
+        await collection.insertOne(data);
+        res.status(200).send({status: 200, message: `Documento con el id ${id} se ha creado Correctamente`});
+    }catch(Error){ 
+        res.status(400).send(Error);
+    }
+}
+
+export const putProductoV101 = async (req,res)=>{
+    let actualizaciones ={...req.body};
+    let filter = parseInt(req.query.id, 10)
+    try{
+        if(!req.rateLimit) return;
+        let working = await collection.updateOne({_id: filter},{$set: actualizaciones});
+        if (working.modifiedCount > 0) {
+            res.status(200).send({status: 200, message: `Documento con el id ${filter} se ha actualizado Correctamente`});
+        } else {
+            working.matchedCount === 1
+            ? res.status(200).send({ status: 200, message:`No se realizaron cambios en el documento con el id ${filter}`})
+            : res.status(404).send({ status: 404, message:`El documento con el id ${filter} no ha sido encontrado`});
+        } 
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+export const deleteProductoV101 = async (req,res)=>{
+    try {
+        if(!req.rateLimit) return;
+        let data = req.body
+        let id = data.id
+        let funtion = await collection.deleteOne({"_id":id})
+        funtion.deletedCount === 1
+        ? res.status(200).send({ status: 200, message:`Documento con el id ${id} ha sido eliminado correctamente`})
+        : res.status(404).send({ status: 404, message:`El documento con el id ${id} no ha sido encontrado`});
+
+    } catch (error) {
+        res.status(400).send(Error);
+    }
+}
