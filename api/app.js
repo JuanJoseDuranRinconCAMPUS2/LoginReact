bb05e985c4a4684151e89aa06e11fe5dcc567275
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-09 17:52:45 -0500

    feat: :sparkles: Rutas de la API
    Creacion de rutas para los diferentes endpoints establecidos

--- api/app.js
+++ api/app.js
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
+import Producto from './routers/Producto.js'
+import AppCrearUsuario from './routers/crearUsuarios.js';
+import AppIngresoUsuario from './routers/ingresarUsuario.js';
 
 console.clear();
 dotenv.config({ path: `./api/.env` });
@@ -9,8 +12,15 @@ loginApi.use(express.json());
 
 //Login
 // ════════ ⋆★⋆ ════════
-
+loginApi.use('/CrearUsuario', AppCrearUsuario);
+loginApi.use('/IngresarUsuario', AppIngresoUsuario);
 // ════════ ⋆★⋆ ════════
+
+//Endpoint
+// ════════ ⋆★⋆ ════════
+loginApi.use('/Producto', Producto);
+// ════════ ⋆★⋆ ════════
+
 
 const config = JSON.parse(process.env.MY_CONFIG);
 
