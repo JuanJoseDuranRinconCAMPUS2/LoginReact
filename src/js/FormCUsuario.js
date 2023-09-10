d261ffde807f25fb041aa8b0fd2d8087a2c93a31
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-10 13:37:02 -0500

    feat: :card_file_box: Filtro dde data del Formulario
    Se crea la funcion que tiene la labor de cambiar los alias de los datos recopilados para hacerlos compatibles con la api creada y luego llama la funcion para ingresar la data a la API

---
+++ src/js/FormCUsuario.js
@@ -0,0 +1,13 @@
+import { newUser } from '../js/services/conCrearU';
+
+export let sendUser = async (User)=>{
+    let { 
+        Username:nombre_Usuario, email:correo_Usuario, 
+        password:contraseña_Usuario , rol:codeRol_Usuario
+    } = User
+    User = {
+        nombre_Usuario, correo_Usuario, contraseña_Usuario, 
+        versiones_Api: ["1.0.1", "1.1.0"], codeRol_Usuario: (codeRol_Usuario == 1) ? "CsWscIpEhqmr1987" : "CsWscYrYwyvemws22501"
+    }
+    newUser(User);
+}
