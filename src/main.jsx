d79dff2fbda4eb2cbbc94ae2609a5d3f45c353e6
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-10 17:33:11 -0500

    style: :lipstick: Estilos y animacion ModalCUsuario
    Se crea el archivo css con los diseños para el modal y sus elementos

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FormCUsuario from './components/formCUsuario'
+import ModalCUsuario from './components/ModalCUsuario'
 
 let app = document.querySelector('#root')
 ReactDOM.createRoot(app).render(
   <React.StrictMode>
     <FormCUsuario/>
+    <ModalCUsuario/>
   </React.StrictMode>,
 )
 
