7f0f11b3630eb44668d217305ac7cc0cc2973804
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-10 11:01:44 -0500

    feat: :zap: Autorizacion CORS
    Se utiliza la dependencia de cors para dar  autorizacion al manejo del endpoint a la url del frondend

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import Producto from './routers/Producto.js'
 import AppCrearUsuario from './routers/crearUsuarios.js';
 import AppIngresoUsuario from './routers/ingresarUsuario.js';
@@ -9,6 +10,18 @@ dotenv.config({ path: `./api/.env` });
 
 const loginApi = express();
 loginApi.use(express.json());
+loginApi.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+loginApi.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'accept-version,Content-Type,Authorization',
+    preflightContinue: false,
+  }));
 
 //Login
 // ════════ ⋆★⋆ ════════
