ea00811c331af24b350e46ba6ebdd76182d2420f
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-10 21:22:05 -0500

    feat: :sparkles: Implementacion del componente ModalCUsuario
    Se implementa este componente en la pagina del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FormCUsuario from './components/formCUsuario'
 import ModalCUsuario from './components/ModalCUsuario'
+import FormIUsuario from './components/FormIUsuario'
 
 let app = document.querySelector('#root')
 ReactDOM.createRoot(app).render(
   <React.StrictMode>
     <FormCUsuario/>
-    <ModalCUsuario/>
   </React.StrictMode>,
 )
 
+let appL = document.querySelector('#login')
+ReactDOM.createRoot(appL).render(
+  <React.StrictMode>
+    <FormIUsuario/>
+  </React.StrictMode>,
+)
+
+let appM = document.querySelector('#modal')
+ReactDOM.createRoot(appM).render(
+  <React.StrictMode>
+    <ModalCUsuario/>
+  </React.StrictMode>,
+)
 
 
