6ed199c9832e5329b97f53edd3e2dde47cfc7ed8
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-10 19:34:37 -0500

    style: :lipstick: Implementacion CSS y boton de contraseñas con imagenes
    se importa el css en el componente y ademas se cambia los textos del boton por iconos que reflejan  el estado de visualizacion del password

--- src/components/FormCUsuario.jsx
+++ src/components/FormCUsuario.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { PropTypes } from 'prop-types';
 import { sendUser } from '../js/FormCUsuario';
-
+import '../css/FormCUsuario.css'
 export default function FormCUsuario() {
 
     useEffect(() => {
@@ -19,11 +19,21 @@ export default function FormCUsuario() {
         const eventoPassword = () => {
             if (password.type == "password") {
                 password.type = "text";
-                showPasswordButton.textContent = 'Hide Password';
+                showPasswordButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
+                </svg>
+                `;
                 return
             } else {
                 password.type = "password";
-                showPasswordButton.textContent = "Show Password";
+                showPasswordButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                </svg>
+                `;
                 return
             }
         };
@@ -46,7 +56,7 @@ export default function FormCUsuario() {
   
     return (
         <>
-            <h2>User Registration</h2>
+            <h2 className='titleUserR'>User Registration</h2>
             <form className="form">
                 <span className="input-span">
                     <label htmlFor="Username" className="label">
@@ -74,12 +84,15 @@ export default function FormCUsuario() {
                         Password
                     </label>
                     <input type="password" name="password" id="password" required/>
-                    <button type="button" id="showPasswordButton">Show Password</button>
                 </span>
+                <button type="button" className="button" id="showPasswordButton"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                </svg></button>
                 <span className="span">
                     <a href="#">Forgot password?</a>
                     </span>
-                    <input className="submit" type="submit" value="Log in" />
+                    <button className="button" type="submit" value="Log in"> Send</button>
                     <span className="span">
                     Don't have an account? <a href="#">Sign up</a>
                 </span>
