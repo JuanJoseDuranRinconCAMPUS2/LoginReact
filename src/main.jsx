f8fa68df900bbf9ea99f988cc66431b76d512a62
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-13 09:27:07 -0500

    feat: :sparkles: Implementacion del componente FormRecoveryPW
    Se crea la implementacion de este componente en el proyecto principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FormCUsuario from './components/formCUsuario'
+import FormCUsuario from './components/FormCUsuario'
 import ModalCUsuario from './components/ModalCUsuario'
 import FormIUsuario from './components/FormIUsuario'
 import CardsProducts from './components/CardsProducts'
+import FormRecoveryPW from './components/FormRecoveryPW'
 
 let app = document.querySelector('#root')
 ReactDOM.createRoot(app).render(
@@ -30,6 +31,13 @@ let appC = document.querySelector('#cards')
 ReactDOM.createRoot(appC).render(
   <React.StrictMode>
     <CardsProducts/>
+  </React.StrictMode>,
+)
+
+let appR = document.querySelector('#formPassword')
+ReactDOM.createRoot(appR).render(
+  <React.StrictMode>
+    <FormRecoveryPW/>
   </React.StrictMode>,
 )
 
