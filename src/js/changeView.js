259f2b4b5d01c234960378a1266b61188056e783
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-13 09:25:34 -0500

    feat: :sparkles: Funciones de cambio de visibilidad
    se agrega el div del formulario de recuperacion de contraseña en las funciones de visibilidad de componentes

--- src/js/changeView.js
+++ src/js/changeView.js
@@ -1,19 +1,39 @@
 export let viewRoot = async ()=>{
     const divLogin = document.querySelector("#login");
     const divRoot = document.querySelector("#root");
+    const divPassword = document.querySelector("#formPassword");
 
     divLogin.style.display = "none";
 
     divRoot.style.display = "block";
-   
+
+    divPassword.style.display = "none";
+
 }
 
 export let viewlogin= async ()=>{
     const divLogin = document.querySelector("#login");
     const divRoot = document.querySelector("#root");
+    const divPassword = document.querySelector("#formPassword");
 
     divLogin.style.display = "block";
 
     divRoot.style.display = "none";
+    
+    divPassword.style.display = "none";
+   
+}
+
+
+export let viewPassword= async ()=>{
+    const divLogin = document.querySelector("#login");
+    const divRoot = document.querySelector("#root");
+    const divPassword = document.querySelector("#formPassword");
+
+    divLogin.style.display = "none";
+
+    divRoot.style.display = "none";
+    
+    divPassword.style.display = "block";
    
 }
