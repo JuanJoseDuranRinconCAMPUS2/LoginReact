e9fc6811e1b5cca1e1350d558b0ef0f04b75535a
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-15 07:47:02 -0500

    feat: :sparkles: Funcion v1.1.0 Actualizar contraseñas
    se crea la funcion del endpoint la cual tiene la funcion de actualizar la contraseña del usuario

---
+++ api/versions/1.1.0/recoveryPasswordv1.1.0.js
@@ -0,0 +1,26 @@
+import { con } from "../../db/atlas.js";
+import errorcontroller from "../../Middlewares/ErroresMongo.js"
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: `./api/.env` });
+let db= await con();
+let collection = db.collection("usuario_Api");
+
+
+export const updatePassword = async (req,res)=>{
+    try{
+        if(!req.rateLimit) return;
+        let data= {...req.body};
+
+        await collection.updateOne({ nombre : data.name, email : data.email },{$set: { password : data.newPassword}});
+
+        res.status(200).send({
+            status: 200, 
+            message: `Contraseña actualizada`, 
+            usuario: data.name
+        });
+    } catch (error) {
+        errorcontroller(error);
+    }
+}
