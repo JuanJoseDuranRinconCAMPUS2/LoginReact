2ca69a710a5cb8a56bb595c600412bfeff845549
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-16 18:48:07 -0500

    feat: :sparkles: Sistema de envio de data y redirecciones
    se crea el sistema que tiene la funcion de enviar la data suministrada por el usuario al siguiente formulario de cambio de contraseñas y ademas se crean las diferentes redirecciones a los apartados de la web

--- src/components/FormRecoveryPW.jsx
+++ src/components/FormRecoveryPW.jsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { PropTypes } from 'prop-types';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { RecoveryUser } from '../js/FormRecoveryPW';
 import '../css/style.css'
 
+let data = {}
 export default function FormRecoveryPW() {
-
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        UsernameR: '',
+        emailR: '',
+    });
     useEffect(() => {
 
         const recovery = document.querySelector(".formRecovery");
-
+        const divPassword = document.querySelector("#sendEmailForm");
 
         const eventoSubmit = (e) => {
             e.preventDefault();
             let data = Object.fromEntries(new FormData(e.target));
+            divPassword.style.display = "none";
+            setFormData(data);
             RecoveryUser(data);
+            navigate('NewPasswordVerification');
             recovery.reset();
         }
 
@@ -30,26 +39,33 @@ export default function FormRecoveryPW() {
   
     return (
         <>
-            <h2 className='titleUserR'>lost your password?</h2>
-            <form className="formRecovery">
-                <span className="span">
-                    enter your username and email to send you an email with your password recovery code
-                </span>
-                <span className="input-span">
-                    <label htmlFor="Username" className="label">
-                        Username
-                    </label>
-                    <input type="Username" name="UsernameR" id="UsernameR" required/>
-                </span>
-                <span className="input-span">
-                    <label htmlFor="email" className="label">
-                        Email
-                    </label>
-                    <input type="email" name="emailR" id="emailR" required/>
-                </span>
-                <button className="button" type="submit" value="recovery P"> Send</button>
-
-        </form>
+            <div id='recoveryPassword'>
+                <div id='sendEmailForm'>
+                    <h2 className='titleUserR'>lost your password?</h2>
+                    <form className="formRecovery">
+                        <span className="span">
+                            enter your username and email to send you an email with your password recovery code
+                        </span>
+                        <span className="input-span">
+                            <label htmlFor="Username" className="label">
+                                Username
+                            </label>
+                            <input type="Username" name="UsernameR" id="UsernameR" required/>
+                        </span>
+                        <span className="input-span">
+                            <label htmlFor="email" className="label">
+                                Email
+                            </label>
+                            <input type="email" name="emailR" id="emailR" required/>
+                        </span>
+                        <button className="button" type="submit" value="recovery P"> Send</button>
+                        <span className="span">
+                            Remember your password? <Link to={`/`} id='Signin'>Sign in</Link>
+                        </span>
+                    </form>
+                </div>
+            <Outlet context={formData}/>
+            </div>
         </>
     )
 }
